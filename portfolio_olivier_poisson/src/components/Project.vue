d29ea4b3fcc4a9a4fcc9c93dbfd327b2dfc808c6
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-04-09 17:36:24 -0400

    Progress

--- portfolio_olivier_poisson/src/components/Project.vue
+++ portfolio_olivier_poisson/src/components/Project.vue
@@ -4,32 +4,61 @@
             <img :src="require(`@/assets/${imgSrc}`)" alt="background image of project" class="background" />
         </div>
         <div class="right">
-            <h1 class="title">project title</h1>
+            <h1 class="title">{{ title }}</h1>
+            <h3 class="subtitle">Technologies used</h3>
             <div class="technologies Row">
-                <div class="technology">
-                    <img src="" alt="" />
-                </div>
-                <div class="technology">
-                    <img src="" alt="" />
-                </div>
-                <div class="technology">
-                    <img src="" alt="" />
-                </div>
-                <div class="technology">
-                    <img src="" alt="" />
+                <div v-for="tech in technologies" :key="tech" class="technology">
+                    <img class="techImg" :src="require(`@/assets/${tech.imgSrc}`)" :alt="tech.Name" />
+                    <p>{{ tech.Name }}</p>
                 </div>
             </div>
             <p class="projectDesc">project description</p>
+            <div class="RowCenter">
+                <button @click="triggerFullScreen" class="viewMore">{{ fullScreen ? "View Less" : "View More" }}</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import gsap from "gsap";
+    const timeline = gsap.timeline({
+        defaults: {
+            duration: 1,
+            ease: "Power2.easeOut",
+        },
+    });
     export default {
         name: "Project",
         props: {
             imgSrc: {
                 type: String,
+            },
+            title: { type: String },
+            technologies: { type: [] },
+        },
+        data() {
+            return {
+                fullScreen: false,
+            };
+        },
+        methods: {
+            triggerFullScreen() {
+                if (!this.fullScreen) {
+                    timeline.to(".container", { height: "100vh" });
+                    timeline.to("body", { overflowY: "hidden" }, "<");
+                    timeline.to(
+                        ".section2",
+                        {
+                            zIndex: 10,
+                            padding: 0,
+                        },
+                        "<"
+                    );
+                    timeline.to(".left", { height: "100%" }, "<");
+                    timeline.to(".right", { height: "100%" }, "<");
+                }
+                this.fullScreen = !this.fullScreen;
             },
         },
     };
@@ -37,19 +66,46 @@
 <style scoped>
     .container {
         margin: 0 auto;
-        width: 80%;
+        height: 80vh;
+        width: 100%;
+        transform-origin: center;
     }
     .left {
         flex: 2;
+        transform-origin: center;
     }
     .right {
         flex: 1;
+        transform-origin: center;
     }
     .technologies {
         flex-wrap: wrap;
+        padding: 10px 0px;
     }
     .technology > img {
         min-width: 100px;
         min-height: 100px;
+        width: 100%;
+    }
+    .technology > p {
+        text-align: center;
+    }
+    .title {
+        text-align: center;
+        font-size: 2.5em;
+    }
+    .subtitle {
+        text-align: center;
+    }
+    .projectDesc {
+        font-size: 1.5em;
+    }
+    .background {
+        width: 100%;
+        height: 100%;
+        border-radius: 20px 0px 0px 20px;
+    }
+    .technology {
+        width: min-content;
     }
 </style>
--- portfolio_olivier_poisson/src/views/Home.vue
+++ portfolio_olivier_poisson/src/views/Home.vue
@@ -2,144 +2,115 @@
     <section class="hero">
         <div class="web">
             <h1 class="titleWeb">Web</h1>
-            <p class="text">
-                I love to help compagnies build amazing and attracting websites
-                that help them get more customers
-            </p>
+            <p class="text">I love to help compagnies build amazing and attracting websites that help them get more customers</p>
         </div>
         <img class="heroImg" src="../assets/Heroimage.png" alt="" />
         <div class="software">
             <h1 class="titleSoft">Software</h1>
             <p class="text">
-                I love to help compagnies build amazing and functionnal
-                softwares so they can give their customers the best experience
-                possible
+                I love to help compagnies build amazing and functionnal softwares so they can give their customers the best experience possible
             </p>
         </div>
     </section>
     <section class="section2">
-        <div
-            class="Project"
-            v-for="project in projects"
-            v-bind:key="project.id"
-        >
-            <Project :imgSrc="project.image" />
+        <div class="Project" v-for="project in projects" v-bind:key="project.id">
+            <Project :title="project.title" :imgSrc="project.image" />
         </div>
     </section>
 </template>
 
 <script>
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import Project from "../components/Project.vue";
-gsap.registerPlugin(ScrollTrigger);
+    import gsap from "gsap";
+    import ScrollTrigger from "gsap/ScrollTrigger";
+    import Project from "../components/Project.vue";
+    gsap.registerPlugin(ScrollTrigger);
 
-export default {
-    name: "Home",
-    data() {
-        return {
-            projects: [
-                {
-                    id: 1,
-                    title: "Movie Website",
-                    image: "logo.png",
-                    description:
-                        "Website used to view and search information about movies, actors",
-                },
-            ],
-        };
-    },
-    components: {
-        Project: Project,
-    },
-    methods: {
-        heroScrollAnimation() {
-            const timeline = gsap.timeline({
-                scrollTrigger: {
-                    trigger: ".hero",
-                    start: "0%",
-                    end: "80%",
-                    scrub: true,
-                },
-            });
-            timeline.fromTo(".hero", { opacity: 1 }, { opacity: 0 });
+    export default {
+        name: "Home",
+        data() {
+            return {
+                projects: [
+                    {
+                        id: 1,
+                        title: "Film Apis",
+                        image: "FilmApis1.png",
+                        description: "Website used to view and search information about movies, actors",
+                    },
+                ],
+            };
         },
-        HeroTextAnimation() {
-            const timeline = gsap.timeline({
-                defaults: {
-                    duration: 1,
-                },
-            });
-            timeline.fromTo(
-                ".heroImg",
-                { opacity: 0 },
-                { opacity: 1, duration: 2, ease: "Power2.easeIn" }
-            );
-            timeline.fromTo(
-                ".titleWeb",
-                { x: 50, opacity: 0 },
-                { x: 0, opacity: 1, ease: "Power2.easeOut" },
-                "<50%"
-            );
-            timeline.fromTo(
-                ".titleSoft",
-                { x: -50, opacity: 0 },
-                { x: 0, opacity: 1, ease: "Power2.easeOut" },
-                "<"
-            );
-            timeline.fromTo(
-                ".text",
-                { y: 50, opacity: 0 },
-                { y: 0, opacity: 1 },
-                "<50%"
-            );
+        components: {
+            Project: Project,
         },
-        setup() {
-            gsap.set(".underline", { width: "0%" });
-            gsap.set(".hero", { zIndex: "-1" });
-            gsap.set(".section2", { zIndex: "2", position: "relative" });
+        methods: {
+            heroScrollAnimation() {
+                const timeline = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: ".hero",
+                        start: "0%",
+                        end: "80%",
+                        scrub: true,
+                    },
+                });
+                timeline.fromTo(".hero", { opacity: 1 }, { opacity: 0 });
+            },
+            HeroTextAnimation() {
+                const timeline = gsap.timeline({
+                    defaults: {
+                        duration: 1,
+                    },
+                });
+                timeline.fromTo(".heroImg", { opacity: 0 }, { opacity: 1, duration: 2, ease: "Power2.easeIn" });
+                timeline.fromTo(".titleWeb", { x: 50, opacity: 0 }, { x: 0, opacity: 1, ease: "Power2.easeOut" }, "<50%");
+                timeline.fromTo(".titleSoft", { x: -50, opacity: 0 }, { x: 0, opacity: 1, ease: "Power2.easeOut" }, "<");
+                timeline.fromTo(".text", { y: 50, opacity: 0 }, { y: 0, opacity: 1 }, "<50%");
+            },
+            setup() {
+                gsap.set(".underline", { width: "0%" });
+                gsap.set(".hero", { zIndex: "-1" });
+                gsap.set(".section2", { zIndex: "2", position: "relative" });
+            },
         },
-    },
-    mounted: function () {
-        this.heroScrollAnimation();
-        this.HeroTextAnimation();
-    },
-};
+        mounted: function () {
+            this.heroScrollAnimation();
+            this.HeroTextAnimation();
+        },
+    };
 </script>
 <style scoped>
-.hero {
-    height: 100vh;
-    display: flex;
-    z-index: 1;
-    justify-content: center;
-    position: sticky;
-    top: 0;
-}
-.heroImg {
-    transform: scale(1.3);
-    transform-origin: bottom;
-}
+    .hero {
+        height: 100vh;
+        display: flex;
+        z-index: 1;
+        justify-content: center;
+        position: sticky;
+        top: 0;
+    }
+    .heroImg {
+        transform: scale(1.3);
+        transform-origin: bottom;
+    }
 
-.web,
-.software {
-    display: flex;
-    align-items: center;
-    flex: 1;
-    flex-direction: column;
-    padding: 0px 50px;
-    justify-content: center;
-    overflow: hidden;
-}
-.titleWeb,
-.titleSoft {
-    font-size: 4em;
-}
-.section2 {
-    z-index: 2;
-    padding: 10vw;
-    position: relative;
-}
-.text {
-    color: #4d4b4b;
-}
+    .web,
+    .software {
+        display: flex;
+        align-items: center;
+        flex: 1;
+        flex-direction: column;
+        padding: 0px 50px;
+        justify-content: center;
+        overflow: hidden;
+    }
+    .titleWeb,
+    .titleSoft {
+        font-size: 4em;
+    }
+    .section2 {
+        z-index: 2;
+        padding: 10vw;
+        position: relative;
+    }
+    .text {
+        color: #4d4b4b;
+    }
 </style>
